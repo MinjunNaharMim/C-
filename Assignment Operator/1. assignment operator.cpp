719fd44e3805c0b4c5ddb4de4476ad49a990adba
MinjunNaharMim <minjun15-12974@diu.edu.bd>
2021-11-26 17:40:54 +0600

    this is six version

---
+++ Assignment Operator/1. assignment operator.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    int x=3;
+    int y=2;
+
+    x +=y; //x=x+y;
+
+    cout<<"X1= "<<x;
+
+    cout<<endl;
+
+    x-=y; //x=x-y;
+
+    cout<<"X2= "<<x;
+
+    cout<<endl;
+
+    x*=y; //x=x*y;
+
+    cout<<"X3= "<<x;
+
+    cout<<endl;
+
+    x/=y; //x=x/y;
+
+    cout<<"X4= "<<x;
+
+    cout<<endl;
+
+    x%=y; //x=x%y;
+
+    cout<<"X5= "<<x;
+
+    getch();
+}
---
+++ Assignment Operator/2. assignment operator value uses.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    int x=3;
+
+    x +=2; //x=x+y;
+
+    cout<<"X1= "<<x;
+
+    cout<<endl;
+
+    x-=4; //x=x-y;
+
+    cout<<"X2= "<<x;
+
+    cout<<endl;
+
+    x*=6; //x=x*y;
+
+    cout<<"X3= "<<x;
+
+    cout<<endl;
+
+    x/=7; //x=x/y;
+
+    cout<<"X4= "<<x;
+
+    cout<<endl;
+
+    x%=10; //x=x%y;
+
+    cout<<"X5= "<<x;
+
+    getch();
+}
+
